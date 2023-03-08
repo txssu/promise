2e2878bc306a10f0b8d2b6f8f300789b5210b563
Evgeny Rybin <z.ribin20@gmail.com>
2023-03-08 13:17:58 +0500

    Revert "Add GuardianDB"
    This reverts commit da8b0e0977561a06d5d5102f506e2d55e75e949e.

--- lib/motivnation/guardian.ex
+++ lib/motivnation/guardian.ex
@@ -19,28 +19,4 @@ defmodule MotivNation.Guardian do
   def resource_from_claims(_claims) do
     {:error, :reason_fro_error}
   end
-
-  def after_encode_and_sign(resource, claims, token, _options) do
-    with {:ok, _} <- Guardian.DB.after_encode_and_sign(resource, claims["typ"], claims, token) do
-      {:ok, token}
-    end
-  end
-
-  def on_verify(claims, token, _options) do
-    with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
-      {:ok, claims}
-    end
-  end
-
-  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
-    with {:ok, _, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
-      {:ok, {old_token, old_claims}, {new_token, new_claims}}
-    end
-  end
-
-  def on_revoke(claims, token, _options) do
-    with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
-      {:ok, claims}
-    end
-  end
 end
