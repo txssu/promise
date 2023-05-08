7f1455dbd62eb3b7f77b7cbd26b4ecf940839562
Evgeny Rybin <z.ribin20@gmail.com>
2023-05-08 16:45:23 +0500

    Remove unused tests

--- test/promise_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PromiseWeb.PageControllerTest do
-  use PromiseWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
