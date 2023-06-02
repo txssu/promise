43d0910a8045f398b25e77502a906c7386d43cd6
Evgeny Rybin <z.ribin20@gmail.com>
2023-06-02 14:02:16 +0500

    Add pulling and pushing to scripts

--- build.sh
+++ build.sh
@@ -12,3 +12,7 @@ echo "Compile assets..."
 mix assets.deploy > /dev/null
 
 mix release --overwrite
+
+docker build . -t ghcr.io/waika28/promise:latest
+
+docker push ghcr.io/waika28/promise:latest
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git pull
+
+docker compose pull
+
 docker compose up -d --remove-orphans
 
 docker exec promise_app /app/bin/migrate
